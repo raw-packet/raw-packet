257ac13f0d26c5eeca8cedd53cfe8dd041b749ac
Ivanov Vladimir <ivanov.vladimir.mail@gmail.com>
2017-05-21 12:11:49 +0300

    Add iterations in dhcp discover sender.

--- send_dhcp_discover_packets.py
+++ send_dhcp_discover_packets.py
@@ -14,16 +14,15 @@ if __name__ == "__main__":
     Base.check_user()
     PACKETS = []
 
-    parser = ArgumentParser(add_help=False)
-    parser.add_argument('-h', '--help', help='Print help', action='store_true')
+    parser = ArgumentParser(description='DHCP Discover raw packet sender')
     parser.add_argument('-i', '--interface', help='Set interface name for send discover packets')
     parser.add_argument('-n', '--notspoofmac', help='Don\'t spoof MAC address', action='store_true')
-    parser.add_argument('-p', '--packets', help='Number of packets (default: 500000)', default=500000)
+    parser.add_argument('-p', '--packets', help='Number of packets (default: 100000)', default=100000)
+    parser.add_argument('-k', '--iterations', help='Number of iterations (default: 100)', default=100)
     args = parser.parse_args()
 
-    if args.help:
-        parser.print_help()
-        exit(1)
+    number_of_packets = int(args.packets)
+    number_of_iterations = int(args.iterations)
 
     current_network_interface = ""
     if args.interface is None:
@@ -67,13 +66,19 @@ if __name__ == "__main__":
 
         current_packet = dhcp.make_discover_packet(SRC_MAC, CLIENT_MAC, REQ_IP, HOST_NAME)
         PACKETS.append(current_packet)
-        count += 1
 
+        count += 1
         if count > count_percent:
             stdout.flush()
             stdout.write(" Complete: " + str(index_percent + 1) + "%   \r")
             index_percent += 1
             count_percent = (count_max / 100) * index_percent
+
+    count = 0
+    count_max = int(args.iterations)
+
+    index_percent = 0
+    count_percent = 0
 
     SOCK = socket(AF_PACKET, SOCK_RAW)
     SOCK.bind((current_network_interface, 0))
@@ -83,12 +88,20 @@ if __name__ == "__main__":
     print "Start sending packets:   " + str(datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
     start_time = time()
 
-    for PACKET in PACKETS:
-        SOCK.send(PACKET)
+    while count < count_max:
+        count += 1
+        if count > count_percent:
+            stdout.flush()
+            stdout.write("Complete:                " + str(index_percent + 1) + "%   \r")
+            index_percent += 1
+            count_percent = (count_max / 100) * index_percent
+
+        for PACKET in PACKETS:
+            SOCK.send(PACKET)
 
     stop_time = time()
-    print "All packets sent:        " + str(datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
+    print "\r\nAll packets sent:        " + str(datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
     SOCK.close()
     delta_time = stop_time - start_time
-    speed = count_max / delta_time
+    speed = (number_of_packets * number_of_iterations) / delta_time
     print "Speed:                   " + str(int(speed)) + " pkt/sec\r\n"
