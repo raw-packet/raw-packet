c27d25b8751c763d15662b1bb3573f6257d9a022
Ivanov Vladimir <ivanov.vladimir.mail@gmail.com>
2018-10-21 02:25:41 +0300

    Add DHCPv6 in protocols list file: sniff_test.py

--- Scripts/Others/sniff_test.py
+++ Scripts/Others/sniff_test.py
@@ -24,14 +24,17 @@ def print_packet(request):
     if 'ARP' in request.keys():
         Base.print_info("ARP packet from: ", request['Ethernet']['source'])
 
+    if 'ICMPv6' in request.keys():
+        Base.print_info("ICMPv6 packet from: ", request['Ethernet']['source'])
+
     if 'DNS' in request.keys():
         Base.print_info("DNS packet from: ", request['Ethernet']['source'])
 
     if 'DHCP' in request.keys():
         Base.print_info("DHCP packet from: ", request['Ethernet']['source'])
 
-    if 'ICMPv6' in request.keys():
-        Base.print_info("ICMPv6 packet from: ", request['Ethernet']['source'])
+    if 'DHCPv6' in request.keys():
+        Base.print_info("DHCPv6 packet from: ", request['Ethernet']['source'])
 
     print(dumps(request, indent=4))
 
@@ -42,13 +45,13 @@ def print_packet(request):
 if __name__ == "__main__":
 
     # region Print info message
-    Base.print_info("Available protocols: ", "Ethernet ARP IP IPv6 ICMPv6 UDP DNS DHCP")
+    Base.print_info("Available protocols: ", "Ethernet ARP IP IPv6 ICMPv6 UDP DNS DHCP DHCPv6")
     Base.print_info("Start test sniffing ...")
     # endregion
 
     # region Start sniffer
     sniff = Sniff_raw()
-    sniff.start(protocols=['ARP', 'IP', 'IPv6', 'ICMPv6', 'UDP', 'DNS', 'DHCP'], prn=print_packet)
+    sniff.start(protocols=['ARP', 'IP', 'IPv6', 'ICMPv6', 'UDP', 'DNS', 'DHCP', 'DHCPv6'], prn=print_packet)
     # endregion
 
 # endregion
