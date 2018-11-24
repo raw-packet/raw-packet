1863d3ef8f587fc3b5304cd2e36dc095e74ab4d2
Ivanov Vladimir <ivanov.vladimir.mail@gmail.com>
2018-11-24 19:04:27 +0300

    Change options network_conflict_creator.py, script: apple_dhcp_mitm.py

--- Scripts/Apple/apple_dhcp_mitm.py
+++ Scripts/Apple/apple_dhcp_mitm.py
@@ -624,7 +624,7 @@ if __name__ == "__main__":
 
                 # Start network_conflict_creator.py script
                 sub.Popen(['python ' + script_dir + '/Scripts/Others/network_conflict_creator.py -i ' +
-                           listen_network_interface + ' -I ' + target_ip_address +
+                           listen_network_interface + ' -T ' + target_ip_address +
                            ' -t ' + target_mac_address + ' -q'], shell=True)
 
             # Exceptions
