0a5fbbf66dac22ef54c2b2e0cdb2d6c95f76ef6f
Ivanov Vladimir <ivanov.vladimir.mail@gmail.com>
2019-05-11 19:23:06 +0300

    Update __status__; script: network_conflict_creator.py

--- Scripts/Others/network_conflict_creator.py
+++ Scripts/Others/network_conflict_creator.py
@@ -41,7 +41,7 @@ __license__ = 'MIT'
 __version__ = '0.0.4'
 __maintainer__ = 'Vladimir Ivanov'
 __email__ = 'ivanov.vladimir.mail@gmail.com'
-__status__ = 'Development'
+__status__ = 'Production'
 # endregion
 
 # region Check user and platform
