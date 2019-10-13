ab67efc2dbfdf892ac6828c3c6f583c72b1a3237
Vladimir Ivanov <ivanov.vladimir.mail@gmail.com>
2019-10-13 18:30:48 +0300

    Update script examples

--- Scripts/Others/examples.py
+++ Scripts/Others/examples.py
@@ -3,7 +3,7 @@
 
 # region Description
 """
-test.py: Test new technique
+examples.py: Raw-packet examples
 Author: Vladimir Ivanov
 License: MIT
 Copyright 2019, Raw-packet Project
@@ -41,26 +41,31 @@ __email__ = 'ivanov.vladimir.mail@gmail.com'
 __status__ = 'Development'
 # endregion
 
-# region Check user, platform and print banner
-eth = RawEthernet()
-arp = RawARP()
-ipv4 = RawIPv4()
-ipv6 = RawIPv6()
-udp = RawUDP()
-
-Base = Base()
-Base.check_user()
-Base.check_platform()
-Base.print_banner()
-# endregion
-
-
 # region Main function
 if __name__ == "__main__":
 
+    # region Check user, platform and print banner
+    Base = Base()
+    Base.check_user()
+    Base.check_platform()
+    Base.print_banner()
+    # endregion
+
+    # region Init Raw-packet classes
+    eth = RawEthernet()
+    arp = RawARP()
+    ipv4 = RawIPv4()
+    ipv6 = RawIPv6()
+    udp = RawUDP()
+    # endregion
+
     try:
+        # region Base functions
         print('\n')
         Base.print_info('Network functions:')
+
+        print('\nNetwork interface settings:')
+        print(Base.get_interface_settings('eth0'))
 
         print('\nMac address:')
         print(Base.get_interface_mac_address('eth0', False))
@@ -75,7 +80,7 @@ if __name__ == "__main__":
         print(Base.make_ipv6_link_address('12:34:56:78:90:ab'))
 
         print('\nipv6 link global address:')
-        print(Base.get_interface_ipv6_glob_address('eth0', False))
+        print(Base.get_interface_ipv6_glob_address('eth0'))
 
         print('\nipv6 global addresses:')
         print(Base.get_interface_ipv6_glob_addresses('eth0'))
@@ -127,12 +132,14 @@ if __name__ == "__main__":
 
         print('\nProcess pid by listen port 80:')
         print(Base.get_process_pid_by_listen_port(80))
+        # endregion
 
+        # region RawEthernet functions
         print('\n')
-        Base.print_info('Test network eth functions:')
+        Base.print_info('Test network Ethernet functions:')
 
         print('\nConvert MAC address string to bytes:')
-        print(eth.convert_mac('01:23:45:67:89:0a', True))
+        print(eth.convert_mac('30:31:32:33:34:35', True))
 
         print('\nConvert bad MAC address string to bytes:')
         print(eth.convert_mac('01123:45:67:89:0a', False))
@@ -149,55 +156,75 @@ if __name__ == "__main__":
         print('\nGet MAC address prefix bytes:')
         print(eth.get_mac_prefix(b'\x01#Eg\x89\n', 3, True))
 
-        print('\nMake eth header:')
-        print(eth.make_header('01:23:45:67:89:0a', '01:23:45:67:89:0a', 2048))
+        print('\nMake Ethernet header:')
+        print(eth.make_header('30:31:32:33:34:35', '36:37:38:39:40:41', 2048))
 
-        print('\nMake eth header bad input:')
+        print('\nMake Ethernet header bad input:')
         print(eth.make_header('01:23:45:67:89:0', '01:23:45:67:89:0a', 2048))
 
-        print('\nParse eth header:')
-        print(eth.parse_header(b'\x01#Eg\x89\n\x01#Eg\x89\n\x08\x00'))
+        print('\nParse Ethernet header:')
+        print(eth.parse_header(b'6789@A012345\x08\x00'))
+        # endregion
 
+        # region RawARP functions
         print('\n')
-        Base.print_info('Test network arp functions:')
+        Base.print_info('Test network ARP functions:')
 
-        print('\nMake arp packet')
+        print('\nMake ARP packet')
         print(arp.make_packet())
 
-        print('\nMake arp packet bad input')
+        print('\nMake ARP packet bad input')
         print(arp.make_packet('01:23:45:67:89:0a', '01:23:45:67:89:0b',
                               '01:23:45:67:89:0aa', '1111', '01:23:45:67:89:b', '2222'))
 
-        print('\nParse arp packet')
+        print('\nParse ARP packet')
         print(arp.parse_packet(b'\x00\x01\x08\x00\x06\x04\x00\x01\x01#Eg\x89\n' +
                                b'\xc0\xa8\x01\x01\x01#Eg\x89\x0b\xc0\xa8\x01\x02'))
 
+        print('\nMake ARP request')
+        print(arp.make_request())
+
+        print('\nParse ARP request')
+        print(arp.parse_packet(b'\x00\x01\x08\x00\x06\x04\x00\x01\x01#Eg\x89\n' +
+                               b'\xc0\xa8\x01\x01\x01#Eg\x89\x0b\xc0\xa8\x01\x02'))
+
+        print('\nMake ARP response')
+        print(arp.make_response())
+
+        print('\nParse ARP response')
+        print(arp.parse_packet(b'\x00\x01\x08\x00\x06\x04\x00\x02\x01#Eg\x89\n' +
+                               b'\xc0\xa8\x01\x01\x01#Eg\x89\x0b\xc0\xa8\x01\x02'))
+        # endregion
+
+        # region RawIPv4 functions
         print('\n')
-        Base.print_info('Test network ipv4 functions:')
+        Base.print_info('Test network IPv4 functions:')
 
-        print('\nGet random ipv4 address:')
-        print(ipv4.get_random_ip())
+        print('\nGet random IPv4 address:')
+        print(ipv4.make_random_ip())
 
-        print('\nMake ipv4 header:')
-        print(ipv4.make_header())
+        print('\nMake IPv4 header:')
+        print(ipv4.make_header('192.168.1.1', '192.168.1.2', 1, 0, 8, 17, 64))
 
-        print('\nMake ipv4 header bad input:')
+        print('\nMake IPv4 header bad input:')
         print(ipv4.make_header('1234', '1234', 65537))
 
-        print('\nParse ipv4 header:')
+        print('\nParse IPv4 header:')
         print(ipv4.parse_header(b'E\x00\x00\x1c\x8d/\x00\x00@\x11jN\xc0\xa8\x01\x01\xc0\xa8\x01\x02'))
 
-        print('\nParse ipv4 header bad input:')
+        print('\nParse IPv4 header bad input:')
         print(ipv4.parse_header(b'\x61\x00\x00\x1c\x8d/\x00\x00@\x11jN\xc0\xa8\x01\x01\xc0\xa8\x01\x02'))
+        # endregion
 
+        # region RawIPv6 functions
         print('\n')
         Base.print_info('Test network ipv6 functions:')
 
         print('\nGet random ipv6 address:')
-        print(ipv6.get_random_ip(1, 'fd00::'))
+        print(ipv6.make_random_ip())
 
         print('\nPack ipv6 address:')
-        print(ipv6.pack_addr('fd12::1'))
+        print(ipv6.pack_addr('3132:3334::1'))
 
         print('\nPack ipv6 address bad input:')
         print(ipv6.pack_addr('fd12:::1', False))
@@ -211,7 +238,9 @@ if __name__ == "__main__":
         print('\nParse ipv6 header:')
         print(ipv6.parse_header(b'`\x00\x00\x00\x00\x08\x11@\xfd\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' +
                                 b'\x00\x01\xfd\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02'))
+        # endregion
 
+        # region RawUDP functions
         print('\n')
         Base.print_info('Test network udp functions:')
 
@@ -229,6 +258,7 @@ if __name__ == "__main__":
 
         print('\nParse UDP header')
         print(udp.parse_header(b'\x14\xe9\x14\xe9\x00\x08\xdc\x07'))
+        # endregion
 
     except KeyboardInterrupt:
         Base.print_info("Exit")
