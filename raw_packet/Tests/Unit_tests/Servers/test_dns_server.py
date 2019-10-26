2b9ae5cddf5e02bb9a033be21a92040b495b633a
Vladimir Ivanov <ivanov.vladimir.mail@gmail.com>
2019-10-27 02:21:02 +0300

    Update

--- raw_packet/Tests/Unit_tests/Servers/test_dns_server.py
+++ raw_packet/Tests/Unit_tests/Servers/test_dns_server.py
@@ -23,9 +23,11 @@ from raw_packet.Servers.dns_server import RawDnsServer
 
 # region Import libraries
 from dns.resolver import Resolver
-from dns.rdatatype import A, AAAA
+from dns.rdatatype import A, AAAA, NS, MX
 from subprocess import Popen, PIPE
-from typing import List
+from typing import List, Dict
+from json import dump
+from os import remove
 import unittest
 # endregion
 
@@ -57,14 +59,28 @@ class DnsServerTest(unittest.TestCase):
     listen_port: int = 53
     fake_ipv4_addresses: List[str] = ['192.168.0.123', '192.168.0.234']
     fake_ipv6_addresses: List[str] = ['fd00::123', 'fd00::234']
+    fake_ns_servers: List[str] = ['ns1.test.com', 'ns2.test.com']
+    fake_mx_servers: List[str] = ['mail1.test.com', 'mail2.test.com']
     fake_domains_regexp: List[str] = ['(test1|test2)\\.google.com', 'evil.com']
     fake_domains: List[str] = ['test1.google.com', 'test2.google.com', 'evil.com']
     no_such_domains: List[str] = ['gooogle.com', 'eviiil.com']
     real_domains: List[str] = ['www.google.com', 'google.com']
     success_domains: List[str] = ['evil.com']
 
-    tm.add_task(dns_server.listen, interface, listen_port, None, None, None, False, fake_ipv4_addresses,
-                fake_ipv6_addresses, fake_domains_regexp, no_such_domains, True, False, success_domains)
+    config_file_name: str = 'config.json'
+    config_fake_domains: List[str] = ['test1.com', 'www.test1.com']
+    config_success_domains: List[str] = ['test2.com', 'www.test2.com']
+    config_no_such_domains: List[str] = ['test3.com', 'www.test3.com']
+    config_content: Dict = {'.*test1.com': {'A': fake_ipv4_addresses[0], 'AAAA': fake_ipv6_addresses,
+                                            'NS': fake_ns_servers[0], 'MX': fake_mx_servers},
+                            '.*test2.com': {'success': True, 'A': 'my ipv4 address', 'AAAA': 'my ipv6 address'},
+                            '.*test3.com': {'no such domain': True}}
+    with open(config_file_name, 'w') as config_file:
+        dump(config_content, config_file)
+
+    tm.add_task(dns_server.listen, interface, listen_port, None, None, None, False,
+                fake_ipv4_addresses, fake_ipv6_addresses, fake_domains_regexp, no_such_domains,
+                True, False, success_domains, config_file_name)
 
     test_ipv4_resolver = Resolver()
     test_ipv6_resolver = Resolver()
@@ -115,5 +131,55 @@ class DnsServerTest(unittest.TestCase):
             self.assertNotEqual(self.result_addresses, self.fake_ipv6_addresses)
             self.assertNotEqual(len(self.result_addresses), 0)
             self.result_addresses.clear()
+
+    def test_resolve_config_domains(self):
+        for config_fake_domain in self.config_fake_domains:
+
+            answers = self.test_ipv6_resolver.query(config_fake_domain, A)
+            for answer in answers:
+                self.result_addresses.append(answer.address)
+            self.assertEqual(self.result_addresses, [self.fake_ipv4_addresses[0]])
+            self.result_addresses.clear()
+
+            answers = self.test_ipv6_resolver.query(config_fake_domain, AAAA)
+            for answer in answers:
+                self.result_addresses.append(answer.address)
+            self.assertEqual(self.result_addresses, self.fake_ipv6_addresses)
+            self.result_addresses.clear()
+
+            answers = self.test_ipv6_resolver.query(config_fake_domain, NS)
+            for answer in answers:
+                self.result_addresses.append(str(answer.target)[:-1])
+            self.assertEqual(self.result_addresses, [self.fake_ns_servers[0]])
+            self.result_addresses.clear()
+
+            answers = self.test_ipv6_resolver.query(config_fake_domain, MX)
+            for answer in answers:
+                self.result_addresses.append(str(answer.exchange)[:-1])
+            self.assertEqual(self.result_addresses, self.fake_mx_servers)
+            self.result_addresses.clear()
+
+        for config_success_domain in self.config_success_domains:
+
+            answers = self.test_ipv6_resolver.query(config_success_domain, A)
+            for answer in answers:
+                self.result_addresses.append(answer.address)
+            self.assertEqual(self.result_addresses, [self.ipv4_address])
+            self.result_addresses.clear()
+
+            answers = self.test_ipv6_resolver.query(config_success_domain, AAAA)
+            for answer in answers:
+                self.result_addresses.append(answer.address)
+            self.assertEqual(self.result_addresses, [self.ipv6_address])
+            self.result_addresses.clear()
+
+        for no_such_domain in self.config_no_such_domains:
+
+            nslookup_process = Popen(['nslookup', no_such_domain, self.ipv6_address], stdout=PIPE, stderr=PIPE)
+            (nslookup_stdout, nslookup_stderr) = nslookup_process.communicate()
+            nslookup_stdout: str = nslookup_stdout.decode('utf-8')
+            self.assertIn('server can\'t find ' + no_such_domain, nslookup_stdout)
+
+        remove(self.config_file_name)
 
 # endregion
