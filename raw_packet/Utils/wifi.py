23b1cc6c869cd3ca96a92722f6e48729306392f0
Vladimir Ivanov <ivanov.vladimir.mail@gmail.com>
2020-03-24 20:28:37 +0300

    Update wifi.py

--- raw_packet/Utils/wifi.py
+++ raw_packet/Utils/wifi.py
@@ -10,7 +10,7 @@ Copyright 2020, Raw-packet Project
 # region Import
 from raw_packet.Utils.base import Base
 from raw_packet.Utils.tm import ThreadManager
-from scapy.all import rdpcap, wrpcap, sendp, sniff, Ether, RadioTap
+from scapy.all import rdpcap, wrpcap, sendp, Ether, RadioTap
 from scapy.all import Dot11, Dot11FCS, Dot11Elt, Dot11EltRSN, Dot11EltRates, Dot11EltMicrosoftWPA
 from scapy.all import Dot11EltVendorSpecific
 from scapy.all import Dot11Beacon, Dot11CCMP, Dot11Deauth, EAPOL
@@ -78,6 +78,7 @@ class WiFi:
         0x04: "CCMP",
         0x05: "WEP-104"
     }
+
     # endregion
 
     # endregion
@@ -88,8 +89,7 @@ class WiFi:
     def __init__(self,
                  wireless_interface,
                  wifi_channel: Union[None, int] = None,
-                 ap_bssid: Union[None, str] = None,
-                 client_mac_address: Union[None, str] = None) -> None:
+                 ap_bssid: Union[None, str] = None) -> None:
         """
         Constructor for WiFi class
         :param wireless_interface: Wireless interface name (example: 'wlan0')
@@ -98,6 +98,10 @@ class WiFi:
         :param client_mac_address: Client MAC address (example: '01:23:45:67:89:0b')
         """
         try:
+            # Set directory with pcap files
+            if self._base.get_platform().startswith('Windows'):
+                self._pcap_directory = 'C:\\Windows\\Temp\\raw-packet\\'
+
             # Check network interface is wireless
             assert self._base.check_network_interface_is_wireless(interface_name=wireless_interface), \
                 'Network interface: ' + self._base.error_text(wireless_interface) + ' is not wireless!'
@@ -120,8 +124,7 @@ class WiFi:
                 self._switch_wifi_channel(channel=wifi_channel)
 
             # Check tshark start
-            assert self._start_tshark(bssid=ap_bssid, client=client_mac_address), \
-                'Failed to start tshark!'
+            assert self._start_tshark(bssid=ap_bssid), 'Failed to start tshark!'
 
             # Create thread for reading pcap files
             self._thread_manager.add_task(self._read_pcap_files)
@@ -133,11 +136,12 @@ class WiFi:
         except AssertionError as Error:
             self._base.print_error(Error.args[0])
             exit(1)
+
     # endregion
 
     # region Enable monitor mode on interface
     def _enable_monitor_mode(self,
-                            wireless_interface: Union[None, str] = None) -> bool:
+                             wireless_interface: Union[None, str] = None) -> bool:
         # Set wireless interface
         if wireless_interface is None:
             wireless_interface = self._interface
@@ -166,10 +170,31 @@ class WiFi:
                 self._base.print_info('Wireless interface: ', wireless_interface, ' already in mode monitor')
                 return True
 
+        # Windows
+        elif self._base.get_platform().startswith('Windows'):
+            wlanhelper: Popen = \
+                Popen('wlanhelper "' + wireless_interface + '" mode', shell=True, stdout=PIPE, stderr=PIPE)
+            wlanhelper_output, wlanhelper_error = wlanhelper.communicate()
+            assert wlanhelper_error == b'', 'Library Npcap not found. ' \
+                                            'Install Npcap: https://nmap.org/npcap/ and ' \
+                                            'Nmap: https://nmap.org/download.html'
+            if b'monitor' in wlanhelper_output:
+                self._base.print_info('Wireless interface: ', wireless_interface, ' already in mode monitor')
+                return True
+            else:
+                wlanhelper: Popen = \
+                    Popen('wlanhelper "' + wireless_interface + '" mode monitor', shell=True, stdout=PIPE, stderr=PIPE)
+                wlanhelper_output, wlanhelper_error = wlanhelper.communicate()
+                if b'Success' in wlanhelper_output:
+                    return True
+                else:
+                    return False
+
         # Other
         else:
             self._base.print_error('This platform: ', self._base.get_platform(), ' is not supported')
             return False
+
     # endregion
 
     # region Switch WiFi channel on interface
@@ -184,16 +209,27 @@ class WiFi:
         elif self._base.get_platform().startswith('Linux'):
             run(['iwconfig ' + self._interface + ' channel ' + str(channel)], shell=True)
 
+        # Windows
+        elif self._base.get_platform().startswith('Windows'):
+            wlanhelper: Popen = Popen('wlanhelper "' + self._interface + '" channel ' + str(channel),
+                                      shell=True, stdout=PIPE, stderr=PIPE)
+            wlanhelper_output, wlanhelper_error = wlanhelper.communicate()
+            if wlanhelper_error != b'' or b'Error' in wlanhelper_output:
+                self._base.print_error('Failed to switch channel: ', str(channel))
+                exit(1)
+
         # Other
         else:
             self._base.print_error('This platform: ', self._base.get_platform(), ' is not supported')
             exit(1)
+
     # endregion
 
     # region Convert MAC address
     @staticmethod
     def _convert_mac(mac_address: str) -> bytes:
         return unhexlify(mac_address.lower().replace(':', ''))
+
     # endregion
 
     # region Parse EAPOL payload
@@ -210,7 +246,7 @@ class WiFi:
             eapol_parsed_payload['key information'] = int(eapol_header[4])
             eapol_parsed_payload['key length'] = int(eapol_header[5])
             eapol_parsed_payload['replay counter'] = int(eapol_header[6])
-    
+
             eapol_parsed_payload['wpa key nonce'] = bytes(packet[17:49])
             eapol_parsed_payload['key iv'] = bytes(packet[49:65])
             eapol_parsed_payload['wpa key rsc'] = bytes(packet[65:73])
@@ -221,12 +257,13 @@ class WiFi:
 
             assert 99 + eapol_parsed_payload['wpa key length'] == len(packet), 'Bad EAPOL payload'
             return eapol_parsed_payload
-        
+
         except IndexError:
             return None
-        
+
         except AssertionError:
             return None
+
     # endregion
 
     # region Analyze 802.11 packet
@@ -241,18 +278,16 @@ class WiFi:
                     packet[Dot11FCS].addr2 == packet[Dot11FCS].addr3 and \
                     packet[RadioTap].dBm_AntSignal > -95:
 
-                # Compare channel from RadioTap header and Beacon tag: Current channel
-                if packet[Dot11EltRates].payload.ID == 3:
-                    assert int.from_bytes(packet[Dot11EltRates].payload.info, 'little') == \
-                           self._wifi_channel_frequencies[packet[RadioTap].ChannelFrequency], \
-                        'Bad channel in RadioTap header'
-
                 # First Beacon frame
                 if packet[Dot11FCS].addr2 not in self.bssids.keys():
                     self.bssids[packet[Dot11FCS].addr2]: \
                         Dict[str, Union[int, float, str, bytes, List[Union[int, str]]]] = dict()
-                    self.bssids[packet[Dot11FCS].addr2]['channel']: int = \
-                        self._wifi_channel_frequencies[packet[RadioTap].ChannelFrequency]
+                    if packet[Dot11EltRates].payload.ID == 3:
+                        self.bssids[packet[Dot11FCS].addr2]['channel']: int = \
+                            int.from_bytes(packet[Dot11EltRates].payload.info, 'little')
+                    else:
+                        self.bssids[packet[Dot11FCS].addr2]['channel']: int = \
+                            self._wifi_channel_frequencies[packet[RadioTap].ChannelFrequency]
                     self.bssids[packet[Dot11FCS].addr2]['packets']: int = 0
                     self.bssids[packet[Dot11FCS].addr2]['clients']: List[str] = list()
                     self.bssids[packet[Dot11FCS].addr2]['signals']: List[int] = list()
@@ -315,23 +350,23 @@ class WiFi:
                 # region Encryption info in beacon
                 if packet.haslayer(Dot11EltRSN) and packet.haslayer(Dot11EltMicrosoftWPA):
                     self.bssids[packet[Dot11FCS].addr2]['enc list'].append('WPA/WPA2')
-                    self.bssids[packet[Dot11FCS].addr2]['auth list'].\
+                    self.bssids[packet[Dot11FCS].addr2]['auth list']. \
                         append(self._akmsuite_types[packet[Dot11EltMicrosoftWPA].akm_suites[0].suite])
-                    self.bssids[packet[Dot11FCS].addr2]['cipher list'].\
+                    self.bssids[packet[Dot11FCS].addr2]['cipher list']. \
                         append(self._cipher_types[packet[Dot11EltMicrosoftWPA].group_cipher_suite[0].cipher])
 
                 elif packet.haslayer(Dot11EltRSN):
                     self.bssids[packet[Dot11FCS].addr2]['enc list'].append('WPA2')
-                    self.bssids[packet[Dot11FCS].addr2]['auth list'].\
+                    self.bssids[packet[Dot11FCS].addr2]['auth list']. \
                         append(self._akmsuite_types[packet[Dot11EltRSN].akm_suites[0].suite])
-                    self.bssids[packet[Dot11FCS].addr2]['cipher list'].\
+                    self.bssids[packet[Dot11FCS].addr2]['cipher list']. \
                         append(self._cipher_types[packet[Dot11EltRSN].group_cipher_suite[0].cipher])
 
                 elif packet.haslayer(Dot11EltMicrosoftWPA):
                     self.bssids[packet[Dot11FCS].addr2]['enc list'].append('WPA')
-                    self.bssids[packet[Dot11FCS].addr2]['auth list'].\
+                    self.bssids[packet[Dot11FCS].addr2]['auth list']. \
                         append(self._akmsuite_types[packet[Dot11EltMicrosoftWPA].akm_suites[0].suite])
-                    self.bssids[packet[Dot11FCS].addr2]['cipher list'].\
+                    self.bssids[packet[Dot11FCS].addr2]['cipher list']. \
                         append(self._cipher_types[packet[Dot11EltMicrosoftWPA].group_cipher_suite[0].cipher])
 
                 elif packet.haslayer(Dot11EltVendorSpecific):
@@ -363,7 +398,7 @@ class WiFi:
                     packet[Dot11FCS].SC == 0 and (packet[Dot11FCS].FCfield.value % 2 == 0) and \
                     packet.haslayer(EAPOL):
 
-                eapol:  Union[None, Dict[str, Union[int, bytes]]] = self._parse_eapol(packet[EAPOL].original)
+                eapol: Union[None, Dict[str, Union[int, bytes]]] = self._parse_eapol(packet[EAPOL].original)
                 bssid: str = packet[Dot11FCS].addr2
                 client: str = packet[Dot11FCS].addr1
 
@@ -392,7 +427,6 @@ class WiFi:
             if packet[Dot11FCS].type == 2 and packet[Dot11FCS].subtype == 8 and \
                     packet[Dot11FCS].SC == 0 and (packet[Dot11FCS].FCfield.value % 2 != 0) and \
                     packet.haslayer(EAPOL):
-
                 eapol: Union[None, Dict[str, Union[int, bytes]]] = self._parse_eapol(packet[EAPOL].original)
                 bssid: str = packet[Dot11FCS].addr1
                 client: str = packet[Dot11FCS].addr2
@@ -432,7 +466,7 @@ class WiFi:
                 # Reserved 256 bytes for Client EAPOL key data
                 hccapx += b''.join(b'\x00' for _ in range(256 - len(self.wpa_handshakes[bssid][client]['eapol'])))
                 self.wpa_handshakes[bssid][client]['hccapx content']: bytes = hccapx
-                self.wpa_handshakes[bssid][client]['hccapx file']: str =  \
+                self.wpa_handshakes[bssid][client]['hccapx file']: str = \
                     'wpa' + str(self.wpa_handshakes[bssid][client]['key version']) + \
                     '_' + bssid.replace(':', '') + \
                     '_' + client.replace(':', '') + \
@@ -508,7 +542,7 @@ class WiFi:
                     hccapx_file.write(self.wpa_handshakes[bssid][client]['hashcat 22000 content'])
                 wrpcap(self.wpa_handshakes[bssid][client]['pcap file'], packet, append=True)
                 # endregion
-                
+
             # endregion
 
         except AssertionError:
@@ -533,7 +567,15 @@ class WiFi:
                       wireless_interface: Union[None, str] = None,
                       pcap_files_directory: Union[None, str] = None,
                       bssid: Union[None, str] = None,
-                      client: Union[None, str] = None) -> bool:
+                      sniff_eapol: bool = False) -> bool:
+        # Check BSSID
+        first_4_bytes_of_bssid: str = ''
+        last_2_bytes_of_bssid: str = ''
+        if bssid is not None:
+            assert self._base.mac_address_validation(mac_address=bssid), 'Bad BSSID: ' + self._base.error_text(bssid)
+            first_4_bytes_of_bssid += '0x' + bssid[0:11].replace(':', '')
+            last_2_bytes_of_bssid += '0x' + bssid[12:17].replace(':', '')
+
         # Set wireless interface
         if wireless_interface is None:
             wireless_interface = self._interface
@@ -545,13 +587,48 @@ class WiFi:
         # Kill all tshark processes
         self._base.kill_process_by_name(process_name='tshark')
 
+        # Set path for tshark
+        if self._base.get_platform().startswith('Windows'):
+            tshark_path: str = '"C:\\Program Files\\Wireshark\\tshark.exe"'
+        else:
+            tshark_path: str = 'tshark'
+
+        # Set tshark command
+        if sniff_eapol:
+            if bssid is None:
+                tshark_command: str = \
+                    tshark_path + ' -y "IEEE802_11_RADIO" -I -i "' + wireless_interface + \
+                    '" -b duration:1 -w ' + pcap_files_directory + \
+                    'sniff.pcap -f "wlan[0:2] == 0x8801 or wlan[0:2] == 0x8802"'
+            else:
+                tshark_command: str = \
+                    tshark_path + ' -y "IEEE802_11_RADIO" -I -i "' + wireless_interface + \
+                    '" -b duration:1 -w ' + pcap_files_directory + \
+                    'sniff.pcap -f "(wlan[0:2] == 0x8801 or wlan[0:2] == 0x8802)' \
+                    ' and wlan[16:4] == ' + first_4_bytes_of_bssid + \
+                    ' and wlan[20:2] == ' + last_2_bytes_of_bssid + '"'
+        else:
+            if bssid is None:
+                tshark_command: str = \
+                    tshark_path + ' -y "IEEE802_11_RADIO" -I -i "' + wireless_interface + \
+                    '" -b duration:1 -w ' + pcap_files_directory + \
+                    'sniff.pcap -f "(wlan type data) or (wlan type mgt subtype beacon)"'
+            else:
+                tshark_command: str = \
+                    tshark_path + ' -y "IEEE802_11_RADIO" -I -i "' + wireless_interface + \
+                    '" -b duration:1 -w ' + pcap_files_directory + \
+                    'sniff.pcap -f "((wlan type data) or (wlan type mgt subtype beacon))' \
+                    ' and wlan[16:4] == ' + first_4_bytes_of_bssid + \
+                    ' and wlan[20:2] == ' + last_2_bytes_of_bssid + '"'
+
         # Run tshark process
-        Popen(['tshark -y "IEEE802_11_RADIO" -I -i ' + wireless_interface +
-               ' -b duration:1 -w ' + pcap_files_directory +
-               'sniff.pcap -f "(wlan type data) or (wlan type mgt subtype beacon)"'],
-              shell=True, stdout=PIPE, stderr=PIPE)
+        if self._base.get_platform().startswith('Windows'):
+            Popen(tshark_command, shell=True, stdout=PIPE, stderr=PIPE)
+        else:
+            Popen([tshark_command], shell=True, stdout=PIPE, stderr=PIPE)
 
         return True
+
     # endregion
 
     # region Read pcap files from directory
@@ -612,6 +689,7 @@ class WiFi:
                     sleep(2)
             else:
                 sleep(2)
+
     # endregion
 
     # endregion
@@ -623,12 +701,14 @@ class WiFi:
     def validate_wifi_channel(wifi_channel: int) -> bool:
         return True if wifi_channel in \
                        [1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 36, 40, 44, 48, 52, 56, 60, 64, 132, 136, 140, 144] else False
+
     # endregion
 
     # region Set WiFi chanel and prohibit switching between channels
     def set_wifi_channel(self, channel: int = 1) -> None:
         self._switch_between_channels = False
         self._switch_wifi_channel(channel=channel)
+
     # endregion
 
     # region Sending deauth packets
