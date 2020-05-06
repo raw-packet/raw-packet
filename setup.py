124d2f4a48acd60df02f9941ca68d0cc3d4e4194
Vladimir Ivanov <ivanov.vladimir.mail@gmail.com>
2020-05-07 02:28:37 +0300

    Update

--- setup.py
+++ setup.py
@@ -1,12 +1,43 @@
-import raw_packet
+# region Description
+"""
+setup.py: Raw-packet Project Setup
+Author: Vladimir Ivanov
+License: MIT
+Copyright 2020, Raw-packet Project
+"""
+# endregion
 
-import setuptools
-import pkg_resources
+# region Import
+from setuptools import setup, find_packages
+# endregion
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+# region Authorship information
+__author__ = 'Vladimir Ivanov'
+__copyright__ = 'Copyright 2020, Raw-packet Project'
+__credits__ = ['']
+__license__ = 'MIT'
+__version__ = '0.2.1'
+__maintainer__ = 'Vladimir Ivanov'
+__email__ = 'ivanov.vladimir.mail@gmail.com'
+__status__ = 'Development'
+# endregion
 
-classifiers = [
+# region Setup
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
+setup(
+    name="raw_packet",
+    version=__version__,
+    author=__author__,
+    author_email=__email__,
+    description="This project implements network protocols such as Ethernet ARP IPv4 UDP TCP DHCPv4 ICMPv4 "
+                "IPv6 DHCPv6 ICMPv6 DNS MDNS on raw socket.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://raw-packet.github.io/",
+    packages=find_packages(),
+    classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Console",
         "Environment :: Console :: Curses",
@@ -23,29 +54,18 @@ classifiers = [
         "Programming Language :: Python :: 3.9",
         "Topic :: Education",
         "Topic :: Security"
-    ]
-
-
-setuptools.setup(
-    name="raw_packet",
-    version=raw_packet.__version__,
-    author=raw_packet.__author__,
-    author_email=raw_packet.__email__,
-    description="This project implements network protocols such as Ethernet ARP IPv4 UDP TCP DHCPv4 ICMPv4 IPv6 DHCPv6 ICMPv6 DNS MDNS on raw socket.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://raw-packet.github.io/",
-    packages=setuptools.find_packages(),
-    classifiers=classifiers,
+    ],
     install_requires=[
         "netifaces", "ipaddress", "netaddr", "scapy", 
         "psutil", "prettytable", "distro", "xmltodict",
-        "paramiko", "npyscreen", "pycryptodomex", "getmac", "colorama" 
+        "paramiko", "npyscreen", "pycryptodomex", "getmac",
+        "colorama", "requests", "user-agents"
     ],
     entry_points={
         'console_scripts': [
             'apple_arp_dos=raw_packet.Scripts.Apple.apple_arp_dos:main',
             'apple_dhcp_server=raw_packet.Scripts.Apple.apple_dhcp_server:main',
+            'apple_mitm=raw_packet.Scripts.Apple.apple_mitm:main',
             'arp_scan=raw_packet.Scripts.ARP.arp_scan:main',
             'arp_spoof=raw_packet.Scripts.ARP.arp_spoof:main',
             'dhcpv4_server=raw_packet.Scripts.DHCPv4.dhcpv4_server:main',
@@ -56,9 +76,11 @@ setuptools.setup(
             'ipv6_spoof=raw_packet.Scripts.IPv6.ipv6_spoof:main',
             'ncc=raw_packet.Scripts.NCC.ncc.py:main',
             'nsc=raw_packet.Scripts.NSC.nsc.py:main',
+            'phishing=raw_packet.Scripts.Phishing.phishing:main',
             'wat=raw_packet.Scripts.WiFi.wat:main'
         ],
     },
     python_requires='>=3.7',
     include_package_data=True,
 )
+# endregion
